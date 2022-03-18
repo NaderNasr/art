01deb598bdaff0c8fdcaf8de9eb5c9643f4da480
Armin <glavovic.armin@gmail.com>
2022-03-18 15:12:16 -0400

    basic navbar with logo and cart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Components/Nabar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     </div>
   );
 }
