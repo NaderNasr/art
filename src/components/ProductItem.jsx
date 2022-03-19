4cfa22ca48dcd7bf9e696c611d7dd3113dbe4cd4
NaderNasr <nadernasr7@gmail.com>
2022-03-19 14:38:38 -0400

    ternary condition for shopping cart if product available

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,6 +1,10 @@
+import { Button } from "@mui/material";
 import React from "react";
 import '../styles/productItem.css';
-// import '@google/model-viewer'
+import AvailableProductsAlert from "./AvailableProductsAlert";
+import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
+import Poster from "../../src/1.gltf"
+import '@google/model-viewer'
 
 const ProductItem = ({ product }) => {
 
@@ -11,15 +15,18 @@ const ProductItem = ({ product }) => {
   // const descriptionStriped = description.replace(regex, "");
 
   return (
-    <>
-      <div>
-      <img  style={{width:'200px'}} alt="" src={product.image?.url}/>
-      <p>{product.name}</p>
-      <ul>
-        <li>{product.is.sold_out ? "Sold out" : product.inventory.available + " units"}</li>
-      </ul>
+    <div>
+      <AvailableProductsAlert product={product} />
+      <img style={{ height: '10em' }} alt="product" src={product.image?.url} />
+      {/* <model-viewer src={Poster} ar ar-placement="wall" ar-modes="webxr scene-viewer quick-look" camera-controls alt="A 3D model of some wall art"></model-viewer> */}
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <p style={{fontSize:'24px'}}>{product.name}</p>
+        {product.is.sold_out ? <></> : <Button startIcon={<ShoppingCartCheckoutIcon />}>Add To Cart</Button>}
       </div>
-    </>
+      <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+        <p>{product.price.formatted_with_code}</p>
+      </div>
+    </div>
   );
 };
 
