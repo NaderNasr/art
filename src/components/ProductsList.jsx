8001d482b1d07b5cba7b3de412e0db44bde3e8d6
NaderNasr <nadernasr7@gmail.com>
2022-03-19 15:53:40 -0400

    passing modal state to product item

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -12,30 +12,36 @@ const Item = styled(Paper)(({ theme }) => ({
   textAlign: 'center',
   color: theme.palette.text.secondary,
   background: "#F6F6F6",
-    '&:hover': {
-      boxShadow: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)'
-    },
+  '&:hover': {
+    boxShadow: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)'
+  },
 }));
 
-const ProductsList = ({ products }) => {
+
+const ProductsList = ({ products, setOpen, open }) => {
   //view the products in browser > inspect > console
   console.log(products)
   return (
     //Material UI Grid
     <div>
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12 }}>
-      {products.map((product) => (
-        <Grid item xs={2} sm={4} md={4} key={product.id}>
-          <Item>
-          <ProductItem
-            product={product}
-          />
-          </Item>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12 }}>
+          {products.map((product, idx) => (
+            <Grid item xs={2} sm={4} md={4} key={product.id}>
+              <Item>
+                {/* Product Item */}
+                <ProductItem
+                  //Modal State
+                  open={open} setOpen={setOpen}
+                  // Commerce Products
+                  product={product}
+                  idx={idx}
+                />
+              </Item>
+            </Grid>
+          ))}
         </Grid>
-      ))}
-      </Grid>
-    </Box>
+      </Box>
     </div>
 
   )
