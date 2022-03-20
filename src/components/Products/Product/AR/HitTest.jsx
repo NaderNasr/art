234c98b808a798893fc1f72c3fc484700dd8eaff
Gerard Compion <gcompion93@gmail.com>
2022-03-20 17:18:50 -0400

    fill out hit test/placement functionality in HitTest component, generate marker plane

--- src/components/Products/Product/AR/HitTest.jsx
+++ src/components/Products/Product/AR/HitTest.jsx
@@ -1,8 +1,36 @@
 import React, { useRef, useState } from 'react';
 import { useHitTest, useInteraction } from '@react-three/xr';
-import { Plane } from '@react-three/drei';
+import { useTexture } from '@react-three/drei';
+import { PlaneGeometry } from 'three';
+import { MeshStandardMaterial } from 'three';
 
 const HitTest = ({ image, dimensions }) => {
+  const [placed, setPlaced] = useState(false);
+  const [placement, setPlacement] = useState([]);
+
+  const ref = useRef();
+  const texture = useTexture(image);
+
+  useInteraction(ref, 'onSelect', () => {
+    if (placed) return;
+    setPlaced(true);
+  });
+
+  useHitTest((hit) => {
+    if (placed) return;
+    hit.decompose(ref.current.position, ref.current.rotation, ref.current.scale);
+    const x = ref.current.position.x;
+    const y = ref.current.position.y;
+    const z = ref.current.position.z;
+    setPlacement([x, y, z]);
+  });
+
+  const Marker = () => (
+    <mesh ref={ref}>
+      <PlaneGeometry args={dimensions} />
+      <MeshStandardMaterial map={texture} opacity={0.5} />
+    </mesh>
+  )
 
 }
 
