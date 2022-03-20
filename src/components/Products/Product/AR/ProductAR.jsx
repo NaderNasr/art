408528c1ad5ed0fab6904e310e8f1ec62987d043
Gerard Compion <gcompion93@gmail.com>
2022-03-20 16:33:19 -0400

    implement function to rescale images to a consistent size

--- src/components/Products/Product/AR/ProductAR.jsx
+++ src/components/Products/Product/AR/ProductAR.jsx
@@ -3,9 +3,27 @@ import { useParams } from 'react-router-dom';
 
 const ProductAR = ({ products }) => {
   const params = useParams();
+  const targetProduct = products.find(product => product.id === params.productId);
+  const image = targetProduct.image.url;
+  const dimensions  = targetProduct.image.image_dimensions;
 
-  const targetProduct = products.find(product => product.id = params.productId);
-  
+  const rescaleImageForAR = (height, width) => {
+    const aspect = height / width;
+    let planeHeight = 0;
+    let planeWidth = 0;
+
+    if (aspect <= 1) {
+      planeWidth = 1;
+      planeHeight = 1 * aspect;
+    } else {
+      planeHeight = 1;
+      planeWidth = 1 * aspect;
+    }
+
+    return [planeWidth, planeHeight];
+  }
+
+  const planeDimensions = rescaleImageForAR(dimensions.height, dimensions.width);
 
   return(
     <p>AR is the future! Current product: {params.productId}</p>
