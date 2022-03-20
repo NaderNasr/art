cbddb52a6186887965dcc3bf9615fca751d63857
Armin <glavovic.armin@gmail.com>
2022-03-20 12:44:29 -0400

    preparing push to main

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
 
+import FormInput from './CustomTextField';
+
 const AddressForm = () => {
   const methods = useForm();
 
@@ -9,9 +11,14 @@ const AddressForm = () => {
     <>
       <Typography variant="h6" gutterBottom>Shipping Address</Typography>
       <FormProvider {...methods}>
-        <form onSubmit={}>
+        <form onSubmit=''>
           <Grid container spacing={3}>   
-
+            <FormInput required name='firstName' label='First Name'/>
+            <FormInput required name='lastName' label='Last Name'/>
+            <FormInput required name='address1' label='Address'/>
+            <FormInput required name='email' label='Email'/>
+            <FormInput required name='City' label='City'/>
+            <FormInput required name='Postal Code' label='Postal Code / Zip'/>
           </Grid>
         </form>
       </FormProvider>
--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -3,15 +3,21 @@ import { TextField, Grid } from '@material-ui/core';
 import { useFormContext, Controller } from 'react-hook-form';
 
 const FormInput = ({ name, label, required }) => {
-  const {control } = useFormContext();
+  const { control } = useFormContext();
 
   return (
-    <Grid item xd={12} sm={6}>
+    <Grid item xs={12} sm={6}>
       <Controller 
-        as={TextField}
         control={control}
-        fullWidth
         name={name}
+        render = {({field}) => (
+          <TextField
+            fullWidth
+            label={label}
+            required
+          />
+        )}
+        fullWidth
         label={label}
         required={required}
       />
