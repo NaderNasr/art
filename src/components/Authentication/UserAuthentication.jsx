50a08940b3d8fce00fed54b49273bce92fa4470b
NaderNasr <nadernasr7@gmail.com>
2022-03-20 17:47:23 -0400

    sign in page and set user email state

--- src/components/Authentication/UserAuthentication.jsx
+++ src/components/Authentication/UserAuthentication.jsx
@@ -1,28 +1,86 @@
-import React from 'react'
-// import Commerce from '@chec/commerce.js';
-// import { useFormControl } from '@mui/material/FormControl';
-import { FormControl, FormHelperText, Input, InputLabel } from '@material-ui/core';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-// const commerce = new Commerce('{your_public_key}');
+const theme = createTheme();
 
+const UserAuthentication = ({ handleAuth, setUserEmail }) => {
+  //   const email = 'nadernasr7@gmail.com';
+//   handleAuth(setUserEmail(email))
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    setUserEmail(data.get('email'))
+  };
 
-
-const UserAuthentication = ({auth, jwt}) => {
-  console.log(jwt)
   return (
-    <>
-      <FormControl>
-        <InputLabel htmlFor="my-input">Name</InputLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" />
-      </FormControl>
-    {<br/>}
-      <FormControl>
-        <InputLabel htmlFor="my-input">Email address</InputLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" />
-        <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
-      </FormControl>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            {<br/>}
+            {<br/>}
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            {<br/>}
+
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default UserAuthentication
