944bb8628a3d795e4792f91b666c97c691d6bba4
Gerard Compion <gcompion93@gmail.com>
2022-03-20 13:18:50 -0400

    fix empty cart rendering

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Typography, Button, Grid } from '@material-ui/core';
+import { Container, Typography, Button, Grid, LinearProgress } from '@material-ui/core';
 import { Link } from 'react-router-dom'
 import makeStyles from './styles';
 import CartItem from './CartItem/CartItem';
@@ -8,7 +8,10 @@ const Cart = ({ cart, onRemoveFromCart, onEmptyCart, onUpdateCartQuantity }) => 
   const classes = makeStyles();
 
   const EmptyCart = () => (
-    <Typography variant="subtitle1">Your shopping cart is empty</Typography>
+    <Typography variant="subtitle1">
+      Your shopping cart is empty
+      {console.log('Empty Cart')}
+    </Typography>
   );
 
   const FilledCart = () => (
@@ -35,14 +38,19 @@ const Cart = ({ cart, onRemoveFromCart, onEmptyCart, onUpdateCartQuantity }) => 
       </div>
     </>
   );
-
-  if (!cart.line_items) return 'Loading...'
+  console.log(cart.line_items);
+  if (!cart.line_items) return(
+    <div>
+      <p>Loading</p>
+      <LinearProgress />
+    </div>
+  )
 
   return (
     <Container>
       <div className={classes.toolbar} />
       <Typography className={classes.title} variant="h3" gutterBottom>Your Shopping Cart</Typography>
-      {!cart.line_items ? <EmptyCart /> : <FilledCart />}
+      {cart.line_items.length === 0 ? <EmptyCart /> : <FilledCart />}
     </Container>
   )
 }
