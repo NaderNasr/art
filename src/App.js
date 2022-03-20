d8b4e56c50f847a7886a236d8f39599fad5e2c9c
NaderNasr <nadernasr7@gmail.com>
2022-03-20 01:46:18 -0400

    added loading animation until fetch products are loaded and code clean up

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import { LinearProgress } from '@mui/material';
 import { Box } from '@mui/system';
 import React, { useEffect, useState } from 'react'
-// import ProductsList from './components/ProductsList'
-// import Navbar from './components/Navbar/Navbar';
 import commerce from './lib/commerce';
-import { Navbar , ProductsList } from './components/'
-import { ThemeProvider, createTheme } from "@material-ui/core/styles";
-
-const theme = createTheme({
-  typography: {
-    h1: {
-      fontSize: "3rem"
-    }
-  },
-});
-
+import { Navbar, ProductsList } from './components/'
 
 const App = () => {
   // use state to fetch products
@@ -22,7 +10,6 @@ const App = () => {
   // loading animation
   const [loading, setLoading] = useState(true);
   // modal state
-  // const [open, setOpen] = React.useState(false);
 
   // use promise to load products
   const fetchProducts = () => {
@@ -37,34 +24,29 @@ const App = () => {
 
   //load products once
   useEffect(() => {
-    // const timer = setInterval(() => {
     fetchProducts();
-    setLoading(false)
-  // }, 100);
-  // return () => {
-  //   clearInterval(timer);
-  // };
+    const timer = setInterval(() => {
+      setLoading(false)
+    }, 2000);
+    return () => clearInterval(timer);
   }, []);
 
   return (
-       <div className="App">
-        <div style={{marginBottom:'100px'}}>
+    <div className="App">
+      <div style={{ marginBottom: '100px' }}>
         <Navbar />
-        </div>
+      </div>
 
       {loading
         ?
         <div>
           <Box sx={{ width: '50%', marginLeft: '25%', marginTop: '25%' }}>
-          <p>Loading</p>
+            <p>Loading</p>
             <LinearProgress />
           </Box>
         </div>
         :
-        <ProductsList products={products}
-        // open={open} setOpen={setOpen}
-
-        />}
+        <ProductsList products={products} />}
     </div>
   )
 }
