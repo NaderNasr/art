ea14d49de6cf6774298ebc16f3fb87679ba31a6e
NaderNasr <nadernasr7@gmail.com>
2022-03-22 16:43:02 -0400

    styling

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -8,7 +8,7 @@ const LandingPage = () => {
   return (
     <>
       <BrowserView>
-        <div style={{display: 'flex', height: '100vh' }}>
+        <div id='toot' style={{display: 'flex', height: '100vh' }}>
           <Banner />
         </div>
           <Intro />
