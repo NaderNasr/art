32176efa6c29d96a8392577be9fb07d098434121
NaderNasr <nadernasr7@gmail.com>
2022-03-23 19:00:30 -0400

    user order pictures

--- src/components/Authentication/userProfile/Profile.jsx
+++ src/components/Authentication/userProfile/Profile.jsx
@@ -15,6 +15,7 @@ const Profile = () => {
   const [allOrders, setAllOrders] = useState(null);
 
   const [productNames, setProductNames] = useState('')
+  const [productImages, setProductImages] = useState('')
 
 
 let { id } = useParams();
@@ -51,32 +52,38 @@ useEffect(() => {
     if (getCustomerOrders) {
       const orders = getCustomerOrders.map((eachCustomer) => eachCustomer.order);
       const lineItems = orders.map((order) => order.line_items)
+      //----------
+      const lineItemProductImages = lineItems.map((lineItem) => (
+        lineItem.map((product) => product.image.url)
+      ))
 
+      console.log('lineItemProductXX: ', lineItemProductImages)
+
+      //----------
       const productNames = lineItems.map((lineItem) => (
         lineItem.map((product) => product.product_name)
       ))
-
-      console.log('ProductNames: ', productNames)
-
       setProductNames(productNames)
+      setProductImages(lineItemProductImages)
+      // console.log('lineItemProduct: ', lineItemProduct)
     }
   }
 
 
 }, [allOrders])
 
-console.log('productNames: ', productNames)
-console.log(userInfo.default_shipping)
+// console.log('productNames: ', productNames)
+// console.log(userInfo.default_shipping)
 
 return (
   <>
+    {/* ----------------------------------User Info----------------------------- */}
+
     <Typography variant="h4" component="div">
       Profile Details
     </Typography>
     {userInfo ? <Card sx={{ minWidth: 275 }}>
       <CardContent>
-        {<br />}
-        {<br />}
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           First Name:    {userInfo.firstname}
         </Typography>
@@ -97,16 +104,14 @@ return (
       </Box>
     </>}
     {/* ----------------------------------Orders----------------------------- */}
-    <br /><br /><br />
+    <br />
     <Divider sx={{ borderBottomWidth: 2 }} />
-    <br /><br /><br />
+    <br />
     <Typography variant="h4" component="div">
       Orders
     </Typography>
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
-        {<br />}
-        {<br />}
 
         {productNames !== '' ?
           productNames.map((productName, id) => (
@@ -119,21 +124,28 @@ return (
               <LinearProgress />
             </Box>
           </>}
-
+          <div>
+          {productImages !== '' ? productImages.map((image, id) => (
+            <img alt='product' src={image} style={{width: '100px'}}/>
+          )) : <>
+            <Box sx={{ width: '100%' }}>
+              <p>Loading</p>
+              <LinearProgress />
+            </Box>
+          </>}
+          </div>
       </CardContent>
     </Card>
     {/* ----------------------------------Address----------------------------- */}
-    <br /><br /><br />
+    <br />
     <Divider sx={{ borderBottomWidth: 2 }} />
-    <br /><br /><br />
+    <br />
 
     <Typography variant="h4" component="div">
       Address
     </Typography>
     {userInfo.default_shipping ? <Card sx={{ minWidth: 275 }}>
       <CardContent>
-        {<br />}
-        {<br />}
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           Postal Code:    {userInfo.default_shipping.postal_zip_code}
         </Typography>
@@ -156,10 +168,6 @@ return (
         <LinearProgress />
       </Box>
     </>}
-    {/* ----------------------------------Orders----------------------------- */}
-    <br /><br /><br />
-    <Divider sx={{ borderBottomWidth: 2 }} />
-    <br /><br /><br />
   </>
 
 )
