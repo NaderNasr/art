585dd3590528689059e79669a65de98ae01e509e
NaderNasr <nadernasr7@gmail.com>
2022-03-23 15:36:25 -0400

    added conditional rendering for cards

--- src/components/Authentication/userProfile/Profile.jsx
+++ src/components/Authentication/userProfile/Profile.jsx
@@ -3,9 +3,11 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
+import { LinearProgress } from '@mui/material';
 
 import { useParams } from 'react-router-dom';
 import commerce from '../../../lib/commerce'
+import { Box } from '@mui/system';
 
 const Profile = () => {
   const [userToken, setUserToken] = useState('');
@@ -15,139 +17,152 @@ const Profile = () => {
   const [productNames, setProductNames] = useState('')
 
 
+let { id } = useParams();
+let jwtToken = { id }
+
+const jwt = () => {
+  commerce.customer.getToken(jwtToken.id)
+    .then((jwt) => setUserToken(jwt))
+    .catch((err) => console.log('JWT ERROR: ', err))
+}
+
+const customerInfo = () => {
+  commerce.customer.about().then((customer) => setUserInfo(customer));
+}
+
+const customerOrder = () => {
+  commerce.customer.getOrders(userInfo.id).then((orders) => setAllOrders(orders));
+}
 
 
-  let { id } = useParams();
-  let jwtToken = { id }
+useEffect(() => {
+  // jwt()
+  customerInfo()
+  customerOrder()
+  // console.log('userInfo: ', userInfo)
+}, [])
 
-  const jwt = () => {
-    commerce.customer.getToken(jwtToken.id)
-      .then((jwt) => setUserToken(jwt))
-      .catch((err) => console.log('JWT ERROR: ', err))
-  }
+// customer order => order => multiple of products => product name
 
-  const customerInfo = () => {
-    commerce.customer.about().then((customer) => setUserInfo(customer));
-  }
+useEffect(() => {
 
-  const customerOrder = () => {
-    commerce.customer.getOrders(userInfo.id).then((orders) => setAllOrders(orders));
-  }
+  if (allOrders) {
+    const getCustomerOrders = allOrders.data
+    if (getCustomerOrders) {
+      const orders = getCustomerOrders.map((eachCustomer) => eachCustomer.order);
+      const lineItems = orders.map((order) => order.line_items)
 
+      const productNames = lineItems.map((lineItem) => (
+        lineItem.map((product) => product.product_name)
+      ))
 
-  useEffect(() => {
-    // jwt()
-    customerInfo()
-    customerOrder()
-    // console.log('userInfo: ', userInfo)
-  }, [])
+      console.log('ProductNames: ', productNames)
 
-  // customer order => order => multiple of products => product name
-
-  useEffect(() => {
-
-    if (allOrders) {
-      const getCustomerOrders = allOrders.data
-      if (getCustomerOrders) {
-        const orders = getCustomerOrders.map((eachCustomer) => eachCustomer.order);
-        const lineItems = orders.map((order) => order.line_items)
-
-        const productNames = lineItems.map((lineItem) => (
-          lineItem.map((product) => product.product_name)
-        ))
-
-        console.log('ProductNames: ', productNames)
-
-        setProductNames(productNames)
-      }
+      setProductNames(productNames)
     }
+  }
 
 
-  }, [allOrders])
+}, [allOrders])
 
-  // console.log('allOrders: ', allOrders.data)
-  console.log('productNames: ', productNames)
-  // console.log('userToken: ', userToken)
-  return (
-    <>
-      <Typography variant="h4" component="div">
-        Profile Details
-      </Typography>
-      <Card sx={{ minWidth: 275 }}>
-        <CardContent>
-          {<br />}
-          {<br />}
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            First Name:    {userInfo.firstname}
-          </Typography>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Last Name:    {userInfo.lastname}
-          </Typography>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Phone Number:    {userInfo.phone}
-          </Typography>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            E-mail:    {userInfo.email}
-          </Typography>
-        </CardContent>
-      </Card>
-      {/* ----------------------------------Orders----------------------------- */}
-      <br /><br /><br />
-      <Divider sx={{ borderBottomWidth: 2 }} />
-      <br /><br /><br />
-      <Typography variant="h4" component="div">
-        Orders
-      </Typography>
-      <Card sx={{ minWidth: 275 }}>
-        <CardContent>
-          {<br />}
-          {<br />}
+console.log('productNames: ', productNames)
+console.log(userInfo.default_shipping)
 
-          {productNames !== '' ?
-            productNames.map((productName, id) => (
-              <Typography key={id} sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                {productName}
-              </Typography>
-            )) : <></>}
+return (
+  <>
+    <Typography variant="h4" component="div">
+      Profile Details
+    </Typography>
+    {userInfo ? <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        {<br />}
+        {<br />}
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          First Name:    {userInfo.firstname}
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Last Name:    {userInfo.lastname}
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Phone Number:    {userInfo.phone}
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          E-mail:    {userInfo.email}
+        </Typography>
+      </CardContent>
+    </Card> : <>
+      <Box sx={{ width: '100%' }}>
+        <p>Loading</p>
+        <LinearProgress />
+      </Box>
+    </>}
+    {/* ----------------------------------Orders----------------------------- */}
+    <br /><br /><br />
+    <Divider sx={{ borderBottomWidth: 2 }} />
+    <br /><br /><br />
+    <Typography variant="h4" component="div">
+      Orders
+    </Typography>
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        {<br />}
+        {<br />}
 
-        </CardContent>
-      </Card>
-      {/* ----------------------------------Address----------------------------- */}
-      <br /><br /><br />
-      <Divider sx={{ borderBottomWidth: 2 }} />
-      <br /><br /><br />
+        {productNames !== '' ?
+          productNames.map((productName, id) => (
+            <Typography key={id} sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+              Order # {id} ---- {productName}
+            </Typography>
+          )) : <>
+            <Box sx={{ width: '100%' }}>
+              <p>Loading</p>
+              <LinearProgress />
+            </Box>
+          </>}
 
-      <Typography variant="h4" component="div">
-        Address
-      </Typography>
-      {/* <Card sx={{ minWidth: 275 }}>
-        <CardContent>
-          {<br />}
-          {<br />}
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Postal Code:    {userInfo.default_shipping.postal_zip_code}
-          </Typography>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Province:    {userInfo.default_shipping.county_state}
-          </Typography>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Country:    {userInfo.default_shipping.country}
-          </Typography>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Street:    {userInfo.default_shipping.street}
-          </Typography>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            City:    {userInfo.default_shipping.town_city}
-          </Typography>
-        </CardContent>
-      </Card> */}
-      {/* ----------------------------------Orders----------------------------- */}
-      <br /><br /><br />
-      <Divider sx={{ borderBottomWidth: 2 }} />
-      <br /><br /><br />
-    </>
+      </CardContent>
+    </Card>
+    {/* ----------------------------------Address----------------------------- */}
+    <br /><br /><br />
+    <Divider sx={{ borderBottomWidth: 2 }} />
+    <br /><br /><br />
 
-  )
+    <Typography variant="h4" component="div">
+      Address
+    </Typography>
+    {userInfo.default_shipping ? <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        {<br />}
+        {<br />}
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Postal Code:    {userInfo.default_shipping.postal_zip_code}
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Province:    {userInfo.default_shipping.county_state}
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Country:    {userInfo.default_shipping.country}
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Street:    {userInfo.default_shipping.street}
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          City:    {userInfo.default_shipping.town_city}
+        </Typography>
+      </CardContent>
+    </Card> : <>
+      <Box sx={{ width: '100%' }}>
+        <p>Loading</p>
+        <LinearProgress />
+      </Box>
+    </>}
+    {/* ----------------------------------Orders----------------------------- */}
+    <br /><br /><br />
+    <Divider sx={{ borderBottomWidth: 2 }} />
+    <br /><br /><br />
+  </>
+
+)
 }
 
 export default Profile
