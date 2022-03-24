17248d3f70538f9592070b84206855fc712ed466
NaderNasr <nadernasr7@gmail.com>
2022-03-24 01:50:10 -0400

    styling for modal change

--- src/components/AvailableProductsAlert.jsx
+++ src/components/AvailableProductsAlert.jsx
@@ -8,18 +8,18 @@ const AvailableProductsAlert = ({ product }) => {
     <>
       {product.is.sold_out && !product.inventory.available
         ?
-        <Tooltip title="SOLD OUT" placement="right">
-          <Alert severity="error" style={{ width: '0px' }}></Alert>
+        <Tooltip title="SOLD OUT" placement="right-end">
+          <Alert severity="error" style={{ width: '4px' }}></Alert>
         </Tooltip>
         :
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
 
           {product.inventory.available < 2
-            ? <Tooltip title="Low Stock" placement="right">
+            ? <Tooltip title="Low Stock" placement="right-end">
               <Alert severity="warning"></Alert>
             </Tooltip>
             :
-            <Tooltip title="In Stock" placement="right">
+            <Tooltip title="In Stock" placement="right-end">
               <Alert severity="success" style={{ width: '0px' }}></Alert>
             </Tooltip>
           }
