e086240bc96a744e52c6a2724134e2491e37757b
NaderNasr <nadernasr7@gmail.com>
2022-03-24 01:50:21 -0400

    styles

--- src/components/Products/Product/styles.js
+++ src/components/Products/Product/styles.js
@@ -4,10 +4,10 @@ import { makeStyles, fade } from '@material-ui/core/styles';
 const drawerWidth = 0;
 
 export default makeStyles((theme) => ({
-  productImage: {
-    display: 'block',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    width: '50%',
-  },
+  // productImage: {
+  //   display: 'block',
+  //   marginLeft: 'auto',
+  //   marginRight: 'auto',
+  //   width: '50%',
+  // },
 }));
