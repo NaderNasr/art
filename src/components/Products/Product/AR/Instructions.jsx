5b8a1dc7a7a9053c7f65953de827735f9eaca136
Gerard Compion <gcompion93@gmail.com>
2022-03-27 13:27:38 -0400

    setup basic instructions component

---
+++ src/components/Products/Product/AR/Instructions.jsx
@@ -0,0 +1,15 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+const Instructions = () => {
+
+  return(
+    <div>
+      <Typography variant='h3'>
+        How to use AR
+      </Typography>
+    </div>
+  )
+}
+
+export default Instructions;
