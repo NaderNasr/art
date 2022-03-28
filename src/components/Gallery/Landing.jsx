05e4870cefda410b1a71df9ef86e3d7e44f800e1
Gerard Compion <gcompion93@gmail.com>
2022-03-27 21:15:17 -0400

    swap around some routing

--- src/components/Gallery/Landing.jsx
+++ src/components/Gallery/Landing.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Typography } from '@mui/material';
 import { BrowserView, MobileView } from 'react-device-detect';
-import makeStyles from './styles'
+import Gallery from './Gallery';
+import Demo from './Demo';
+import makeStyles from './styles';
 
-const Landing = () => {
+const Landing = ({ products }) => {
   const classes = makeStyles();
 
   return (
     <>
+      <Gallery products={products} />
       <MobileView>
         <Typography variant="h3">
           The Future of Lorem Ipsum is Here
@@ -16,6 +19,7 @@ const Landing = () => {
           <Typography component="div">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </Typography>
+          <Demo />
         </Box>
       </MobileView>
       <BrowserView>
