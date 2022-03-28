8e5cf6c9ce7c64942c35d7873de01f16f93b019e
Gerard Compion <gcompion93@gmail.com>
2022-03-28 15:36:56 -0400

    remove unused landing page/demo/gif file, explore possibility of one sided planes for exhibits

--- src/components/Gallery/Exhibit.jsx
+++ src/components/Gallery/Exhibit.jsx
@@ -9,7 +9,8 @@ const Exhibit = ({ position, dimensions, image, rotation }) => {
   return(
     <mesh position={position} rotation={rotation}>
       <planeGeometry args={dimensions} />
-      <meshStandardMaterial map={testTexture} side={DoubleSide} />
+      {/* <meshStandardMaterial map={testTexture} side={DoubleSide} /> */}
+      <meshStandardMaterial map={testTexture} />
     </mesh>
   )
 }
