413fdf98e650237d6840fa26487819d6908bad3c
Gerard Compion <gcompion93@gmail.com>
2022-03-27 21:07:28 -0400

    begin setup of basic landing page

---
+++ src/components/Gallery/Landing.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+import { BrowserView, MobileView } from 'react-device-detect';
+import makeStyles from './styles'
+
+const Landing = () => {
+  const classes = makeStyles();
+
+  return (
+    <>
+      <MobileView>
+        <Typography variant="h3">
+          The Future of Lorem Ipsum is Here
+        </Typography>
+        <Box>
+          <Typography component="div">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </Typography>
+        </Box>
+      </MobileView>
+      <BrowserView>
+
+      </BrowserView>
+    </>
+  )
+}
+
+export default Landing;
