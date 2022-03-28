4cd8d0f7d6dc5c4ac2c06c079dfbea86bc45b017
NaderNasr <nadernasr7@gmail.com>
2022-03-28 15:51:57 -0400

    landing page still in progress

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,28 +1,52 @@
 import React from 'react'
 import Sally from '../../assets/Sally.png'
 import SallyBG from '../../assets/SallyBG.svg'
-import Bob from '../../assets/Bob.svg'
-
+import Demo from '../../assets/Demo.gif'
+import titleTwo from '../../assets/title2.svg'
+import Button from '../../assets/Button/Large.svg'
 
 import './styles.css'
+import Grid from '@mui/material/Grid';
+
 
 const Landing = () => {
   return (
     <>
-      <div className='yellowCircle'></div>
-      <div className='yellowCircle_2'></div>
-      <div className='yellowCircle_3'></div>
+
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <div className='yellowCircle'></div>
+          <div className='yellowCircle_2'></div>
+          <div className='yellowCircle_3'></div>
+
+          <div className='redCircle'></div>
+          <div className='redCircle_2'></div>
+
+          <div>
+            <p className='title'>Find your ARt</p>
+            <p className='desc'>Have confidence buying art online</p>
+            <a href='/'><img src={Button} className='button' alt='button' /></a>
+          </div>
+        </Grid>
+        <Grid item xs={4}>
+          <img className='sallybg' src={SallyBG} alt='Sally AR' />
+          <img className='bob' src={Demo} alt='bob AR' />
+          <div className='blueCircle'></div>
+        </Grid>
+        {/* <p className='howItWorks'>How it works</p>
+
+        <Grid item xs={4}>
+
+          <img className='sally' src={Sally} alt='Sally AR' />
+        </Grid>
+        <Grid item xs={8}>
+          <img src={titleTwo} alt='second title' className='title_2' />
+
+        </Grid> */}
+      </Grid>
 
 
-      <div className='redCircle'></div>
 
-      <p className='title'>Find your ARt</p>
-      <p className='desc'>Buy artwork directly from artists or advertise your own works to potential buyers!</p>
-      <img className='sallybg' src={SallyBG} alt='Sally AR' />
-      <img className='sally' src={Sally} alt='Sally AR' />
-
-      <p className='howItWorks'>How it works</p>
-      <img className='bob' src={Bob} alt='Sally AR' />
 
     </>
   )
