285e64ae6a7754d781607ed8679da7c1f7fbc6f4
Gerard Compion <gcompion93@gmail.com>
2022-03-28 15:14:07 -0400

    remove unneeded imports/etc. replaced by moving rotating display into its own component

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,6 +1,5 @@
-import React, { Suspense, useRef } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
 import { useMediaQuery } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import Exhibit from './Exhibit';
@@ -75,7 +74,7 @@ const Gallery = ({ products }) => {
         style={isMobile ? { height: '25em' } : { height: '12em' }}
         camera={{ position: [0, 0, -6] }}
       >
-        <color attach="background" args={['black']} />
+        <color attach="background" args={['#E8E7E6']} />
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 5]} intensity={0.5} />
         <directionalLight position={[-10, 10, 5]} intensity={0.5} />
